915c6eff4e456e5e38906878aa804eb08dddaaf5
gitiann <jaankalder@gmail.com>
2019-01-30 14:05:37 +0200

    removed line which runs test fn. Declared class outside for cycle.

--- estnltk/tests/test_wordnet_db/test_wn.py
+++ estnltk/tests/test_wordnet_db/test_wn.py
@@ -18,8 +18,9 @@ idList = [2,9,815,12,13,14,15,16,17,18]
 
 def test_relations():
     j=0
+    sset = Synset
     for i in idList:
-        relations = Synset(wn,i).get_related_synset()
+        relations = sset(wn,i).get_related_synset()
         if relations[0][0].name is None:
             assert 'None' == web_start_synset[j]
             assert 'None' == web_relation[j]
@@ -30,5 +31,3 @@ def test_relations():
             assert relation[0].name[0:-4] == web_start_synset[j]
             assert relation[1] == web_relation[j]
             j+=1
-
-test_relations()
