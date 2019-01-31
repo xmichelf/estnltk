d0cffa18ebf00de5bdcc21099127027af47e6468
gitiann <jaankalder@gmail.com>
2019-01-31 13:55:17 +0200

    changed web content in form of dictionary for readability in wn test.

--- estnltk/tests/test_wordnet_db/test_wn.py
+++ estnltk/tests/test_wordnet_db/test_wn.py
@@ -7,27 +7,39 @@ from estnltk.wordnet import Synset
 
 wn = Wordnet(version='74')
 
-web_start_synset = ['käitumisakt', 'patutegu', 'väärdumine', 'kollane', 'None', 'puhklema','reduline','pakku minema',
-                             'redu', 'putkaja', 'piss', 'tennisisti küünarliiges', 'kats', 'tundehellus', 'atribuut', 'joonistama']
+source_relation = [ 'patustus', 'päevalillekollane', 'õigusetu', 'puhiseja', 'pageja', 'miktsioonuriin',  'Millsi test', 'liigutustundlikkus', 'limaskestabarjäär', 'tuššima']
+target_relations = {}
+for relation in source_relation:
+    target_relations[relation] = {}
+target_relations['patustus']['hyponym'] = {'käitumisakt'}
+target_relations['patustus']['involved'] =  {'patutegu'}
+target_relations['patustus']['hypernym'] = {'väärdumine'}
+target_relations['päevalillekollane']['hypernym'] = {'kollane'}
+target_relations['õigusetu']['None'] = {'None'}
+target_relations['puhiseja']['agent'] = {'puhklema'}
+target_relations['pageja']['similar'] = {'reduline'}
+target_relations['pageja']['agent'] = {'pakku minema','redu'}
+target_relations['pageja']['hyponym'] = {'putkaja'}
+target_relations['miktsioonuriin']['hypernym'] = {'piss'}
+target_relations['Millsi test']['involved_patient'] = {'tennisisti küünarliiges'}
+target_relations['Millsi test']['hypernym'] = {'kats'}
+target_relations['liigutustundlikkus']['hypernym'] = {'tundehellus'}
+target_relations['limaskestabarjäär']['hypernym'] = {'atribuut'}
+target_relations['tuššima']['hypernym'] = {'joonistama'}
 
-web_relation = ['hypernym', 'involved', 'hyponym', 'hypernym', 'None', 'agent', 'similar', 'agent',
-                     'agent', 'hyponym', 'hypernym', 'involved_patient', 'hypernym', 'hypernym', 'hypernym', 'hypernym']
-
-idList = [2,9,815,12,13,14,15,16,17,18]
-
-
+sourceId = [2,9,815,12,13,14,15,16,17,18]
 def test_relations():
-    j=0
-    sset = Synset
-    for i in idList:
-        relations = sset(wn,i).get_related_synset()
-        if relations[0][0].name is None:
-            assert 'None' == web_start_synset[j]
-            assert 'None' == web_relation[j]
-            j+=1
+    for i in sourceId:
+        source_name = Synset(wn, i).name[:-4]
+        relations = Synset(wn,i).get_related_synset()
+        if relations is None:
+            assert 'None' in target_relations[source_name][rel_type]
             continue
             
         for relation in relations:
-            assert relation[0].name[0:-4] == web_start_synset[j]
-            assert relation[1] == web_relation[j]
-            j+=1
+            if relation[0].name is None:
+                assert 'None' in target_relations[source_name]['None']
+                continue
+            rel_name = relation[0].name[0:-4]
+            rel_type = relation[1]
+            assert rel_name in target_relations[source_name][rel_type]
