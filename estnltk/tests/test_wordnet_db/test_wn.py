a208c999a6c72ef6bc1f0a1238c14feb734ab64d
gitiann <jaankalder@gmail.com>
2019-02-07 13:50:24 +0200

    made root_hypernyms and closure test.Closure not complete yet, problem with closure() in synset.py.

--- estnltk/tests/test_wordnet_db/test_wn.py
+++ estnltk/tests/test_wordnet_db/test_wn.py
@@ -2,7 +2,7 @@
 Comparison of wordnet relations with https://teksaurus.keeleressursid.ee relations.
 '''
 
-from estnltk.wordnet import Wordnet
+from estnltk import wordnet
 from estnltk.wordnet import Synset
 
 source_relation = [ 'patustus', 'päevalillekollane', 'õigusetu', 'puhiseja', 'pageja', 'miktsioonuriin',  'Millsi test', 'liigutustundlikkus', 'limaskestabarjäär', 'tuššima']
@@ -13,7 +13,7 @@ target_relations['patustus']['hyponym'] = {'käitumisakt'}
 target_relations['patustus']['involved'] =  {'patutegu'}
 target_relations['patustus']['hypernym'] = {'väärdumine'}
 target_relations['päevalillekollane']['hypernym'] = {'kollane'}
-target_relations['õigusetu']['None'] = {'None'}
+target_relations['õigusetu'][None] = {None}
 target_relations['puhiseja']['agent'] = {'puhklema'}
 target_relations['pageja']['similar'] = {'reduline'}
 target_relations['pageja']['agent'] = {'pakku minema','redu'}
@@ -25,6 +25,7 @@ target_relations['liigutustundlikkus']['hypernym'] = {'tundehellus'}
 target_relations['limaskestabarjäär']['hypernym'] = {'atribuut'}
 target_relations['tuššima']['hypernym'] = {'joonistama'}
 
+
 wn = Wordnet(version='74')
 
 def test_relations():
@@ -34,8 +35,52 @@ def test_relations():
         relations = Synset(wn, id).get_related_synset() 
         for relation in relations:
             if relation[0].name is None:
-                assert 'None' in target_relations[source_name]['None']
+                assert None in target_relations[source_name][None]
                 continue
             rel_name = relation[0].name[0:-4]
             rel_type = relation[1]
             assert rel_name in target_relations[source_name][rel_type]
+
+source_nodes = ['õigusetu', 'väikettevõtja']
+iteration_relations = {}
+for key in source_nodes:
+    iteration_relations[key] = {}
+    iteration_depth[key] = {}
+
+iteration_relations['õigusetu'][None] = {None}
+iteration_relations['väikettevõtja']['hypernym'] = {'olev', 'miski'} # olev  - 6
+
+iteration_depth['õigusetu']['depth'] = {None}
+iteration_depth['väikettevõtja']['depth'] = {6}
+
+
+def test_root_hypernyms():
+
+    #root_hypernyms test
+    for source_node in source_nodes:
+        id = wn.get_synset(source_node)[0]
+        roots = Synset(wn,id).root_hypernyms(depth_threshold=float('inf'), return_depths=True)
+        for relation in roots:
+            synset = relation[0]
+            depth = relation[1]
+            if synset is None: #if depth is 0, ie. no relations
+                assert None in iteration_relations[source_node][None] 
+            else:
+                assert synset.name[:-4] in iteration_relations[source_node]['hypernym']
+                assert depth in iteration_depth[source_node]['depth']
+
+
+def test_closure():
+    #closure test
+    for source_node in source_nodes:
+        id = wn.get_synset(source_node)[0]
+        roots = Synset(wn, id).root_hypernyms(relation=None, depth_threshold=float('inf'), return_depths=True)
+        for relation in roots:
+            synset = relation[0]
+            depth = relation[1]
+            if synset is None:
+                assert None in iteration_relations[source_node][None]
+            else:
+                assert synset.name[:-4] in iteration_relations[source_node]['hypernym']
+                assert depth in iteration_depth[source_node]['depth']
+
